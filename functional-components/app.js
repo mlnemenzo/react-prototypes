7b9d648e38e67b4b1d2e99fa14d0f12ae13407d7
marknemenzo <mlnemenzo@gmail.com>
2018-04-26 18:04:32 -0700

    put files in new directory

---
+++ functional-components/app.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const App = () => (
+    <div className ="app">
+        <table>
+            <tr>
+                <ul>Name</ul>
+                <ul>Course</ul>
+                <ul>Grade</ul>
+            </tr>
+            <tr>
+                <ul>Peter Parker</ul>
+                <ul>Intro to Photography</ul>
+                <ul>90</ul>
+            </tr>
+            <tr>
+                <ul>Bruce Banner</ul>
+                <ul>Theoretical Physics</ul>
+                <ul>92</ul>
+            </tr>
+            <tr>
+                <ul>Logan</ul>
+                <ul>Lumber Jack</ul>
+                <ul>100</ul>
+            </tr>
+        </table>
+    </div>
+);
+
+export default app;
