b935a7eb5c65a55c0a5c37a3e4eada757ad6c661
marknemenzo <mlnemenzo@gmail.com>
2018-04-30 15:06:14 -0700

    forms return

--- src/components/contact_list.js
+++ src/components/contact_list.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import ContactCard from './contact_card';
+import contactData from '../data/contacts';
 
 class ContactList extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            contacts : contactData
+        }
+    }
+
     render () {
         return <h1>Contact List</h1>
     }
-
-
 }
 
 export default ContactList;
