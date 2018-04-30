9729a96fd12ac902e820812f217161e2d4658874
marknemenzo <mlnemenzo@gmail.com>
2018-04-30 11:34:59 -0700

    init

---
+++ src/components/contact_card.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default props => {
+    return <h1>Full name: {props.firstName} {props.lastName}</h1>
+}
---
+++ src/components/contact_list.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ContactList extends Component {
+    render () {
+        return <h1>Contact List</h1>
+    }
+
+
+}
+
+export default ContactList;
+
